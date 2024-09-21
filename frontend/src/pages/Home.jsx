a8f7207aed8f960e0eca26ccce4c3cca96f43652
Sagar <deshpandesagar187@gmail.com>
2024-09-21 17:32:39 +0530

    Updated home, searchbar and created services folder with service-card and service-list files

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -6,47 +6,65 @@ import heroVideo from '../assets/images/hero-video.mp4';
 import worldImg from '../assets/images/world.png';
 import '../styles/Home.css';
 import Subtitle from '../shared/Subtitle';
+import SearchBar from '../shared/SearchBar';
+import ServiceList from '../services/ServiceList';
 
 const Home = () => {
     return (
         <>
-            <Container>
-                <Row>
-                    <Col lg='6'>
-                        <div className="hero_content">
-                            <div className="hero_subtitle d-flex align-items-center">
-                                <Subtitle subtitle={'Know Before You Go'} />
-                                <img src={worldImg} alt="" />
+            <section>
+                <Container>
+                    <Row>
+                        <Col lg='6'>
+                            <div className="hero_content">
+                                <div className="hero_subtitle d-flex align-items-center">
+                                    <Subtitle subtitle={'Know Before You Go'} />
+                                    <img src={worldImg} alt="" />
+                                </div>
+                                <h1>Traveling opens door to creating
+                                    <span className="highlight"> memories</span>
+                                </h1>
+                                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corrupti repudiandae fuga dolores aut tenetur,
+                                    accusamus reprehenderit dignissimos, velit nihil ut ducimus ipsam esse cum maxime ab quas delectus possimus similique.
+                                    Natus eligendi officiis, perspiciatis molestias ducimus similique ipsa necessitatibus quas nihil sapiente et enim veniam
+                                    numquam expedita, ratione aliquid voluptate, corrupti autem quam ut iure. </p>
                             </div>
-                            <h1>Traveling opens door to creating
-                                <span className="highlight"> memories</span>
-                            </h1>
-                            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corrupti repudiandae fuga dolores aut tenetur,
-                                accusamus reprehenderit dignissimos, velit nihil ut ducimus ipsam esse cum maxime ab quas delectus possimus similique.
-                                Natus eligendi officiis, perspiciatis molestias ducimus similique ipsa necessitatibus quas nihil sapiente et enim veniam
-                                numquam expedita, ratione aliquid voluptate, corrupti autem quam ut iure. </p>
-                        </div>
-                    </Col>
+                        </Col>
 
-                    <Col lg='2'>
-                        <div className="hero_img-box">
-                            <img src={heroImg01} alt="" />
-                        </div>
-                    </Col>
+                        <Col lg='2'>
+                            <div className="hero_img-box">
+                                <img src={heroImg01} alt="" />
+                            </div>
+                        </Col>
 
-                    <Col lg='2'>
-                        <div className="hero_img-box mt-4">
-                            <video src={heroVideo} alt="" controls />
-                        </div>
-                    </Col>
+                        <Col lg='2'>
+                            <div className="hero_img-box mt-4">
+                                <video src={heroVideo} alt="" controls />
+                            </div>
+                        </Col>
 
-                    <Col lg='2'>
-                        <div className="hero_img-box mt-5">
-                            <img src={heroImg02} alt="" />
-                        </div>
-                    </Col>
-                </Row>
-            </Container>
+                        <Col lg='2'>
+                            <div className="hero_img-box mt-5">
+                                <img src={heroImg02} alt="" />
+                            </div>
+                        </Col>
+
+                        <SearchBar />
+                    </Row>
+                </Container>
+            </section>
+
+            <section>
+                <Container>
+                    <Row>
+                        <Col lg='3'>
+                            <h5 className="services__subtitle">What we serve</h5>
+                            <h2 className="services__title">We offer our best services</h2>
+                        </Col>
+                        <ServiceList />
+                    </Row>
+                </Container>
+            </section>
         </>
     );
 };
---
+++ frontend/src/services/ServiceList.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import ServiceCard from './ServiceCard';
+import { Col } from 'reactstrap';
+import WeatherImg from '../assets/images/weather.png';
+import GuideImg from '../assets/images/guide.png';
+import CustomizationImg from '../assets/images/customization.png';
+
+const servicesData = [
+    {
+        imgUrl: WeatherImg,
+        title: "Calculate Weather",
+        desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit",
+    },
+    {
+        imgUrl: GuideImg,
+        title: "Best Tour Guide",
+        desc: "Explicabo eligendi voluptatem eos provident ullam architecto corrupti",
+    },
+    {
+        imgUrl: CustomizationImg,
+        title: "Customization",
+        desc: "Animi eius illo provident pariatur quis explicabo dolores obcaecati sit!",
+    },
+];
+
+const ServiceList = () => {
+    return (
+        <>
+            {
+                servicesData.map((item, index) => (
+                    <Col lg='3' key={index}>
+                        <ServiceCard item={item} />
+                    </Col>
+                ))
+            }
+        </>
+    );
+};
+
+export default ServiceList;
