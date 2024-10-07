190a130bc84c8f278bf2cc6b0369da76c188aa83
Sagar <deshpandesagar187@gmail.com>
2024-10-07 18:47:39 +0530

    created tours and toursDetails page

--- frontend/src/pages/TourDetails.jsx
+++ frontend/src/pages/TourDetails.jsx
@@ -1,9 +1,49 @@
 import React from 'react';
+import '../styles/Tour-details.css';
+import { Container, Col, Row, Form, ListGroup } from 'reactstrap';
+import { useParams } from 'react-router-dom';
+import tourData from '../assets/data/tours';
+import calculateAvgRating from '../utils/avgRating';
 
 const TourDetails = () => {
+
+    const { id } = useParams();
+
+    const tour = tourData.find((tour) => tour.id === id);
+    const { photo, title, desc, price, reviews, address, city, distance, maxGroupSize } = tour;
+
+    const { totalRating, avgRating } = calculateAvgRating(reviews);
+
     return (
-        <div>TourDetails</div>
-    )
-}
+        <>
+            <Container>
+                <Row>
+                    <Col lg='8'>
+                        <div className="tour__content">
+                            <img src={photo} alt="" />
+
+                            <div className="tour__info">
+                                <h2>{title}</h2>
+
+                                <div className='d-flex align-items-center gap-5'>
+                                    <span className="tour__rating d-flex align-items-center gap-1">
+                                        <i className="ri-star-fill" style={{ color: 'var(--secondary-color)' }}></i>
+                                        {calculateAvgRating === 0 ? null : avgRating}
+
+                                        {totalRating === 0 ? (
+                                            "Not rated"
+                                        ) : (
+                                            <span>({reviews.length})</span>
+                                        )}
+                                    </span>
+                                </div>
+                            </div>
+                        </div>
+                    </Col>
+                </Row>
+            </Container>
+        </>
+    );
+};
 
 export default TourDetails;
