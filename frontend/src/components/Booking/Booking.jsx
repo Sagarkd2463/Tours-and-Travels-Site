21317fcec956c51cf3d0218b85dbc3bd2016fe94
Sagar <deshpandesagar187@gmail.com>
2024-10-14 17:31:57 +0530

    created login, register, thankyou pages and updated booking component

--- frontend/src/components/Booking/Booking.jsx
+++ frontend/src/components/Booking/Booking.jsx
@@ -1,13 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../../styles/Booking.css';
 import { Form, FormGroup, ListGroup, ListGroupItem, Button } from 'reactstrap';
+import { useNavigate } from 'react-router-dom';
 
 const Booking = ({ tour, avgRating }) => {
 
     const { price, reviews } = tour;
+    const navigate = useNavigate();
+
+    const serviceFee = 10;
+    const totalAmount = Number(price) * Number(credentials.guestSize) + Number(serviceFee);
+
+    const [credentials, setCredentials] = useState({
+        userId: '01',
+        userEmail: 'example@gmail.com',
+        fullName: '',
+        phone: '',
+        guestSize: 1,
+        bookAt: '',
+    });
 
     const handleChange = (e) => {
+        setCredentials((prev) => ({
+            ...prev,
+            [e.target.id]: e.target.value
+        }));
+    };
 
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        navigate('/thank-you');
     };
 
     return (
@@ -23,7 +46,7 @@ const Booking = ({ tour, avgRating }) => {
 
             <div className="booking__form">
                 <h5>Information</h5>
-                <Form className='booking__info-form'>
+                <Form className='booking__info-form' onSubmit={handleSubmit}>
                     <FormGroup>
                         <input
                             type="text"
@@ -58,6 +81,36 @@ const Booking = ({ tour, avgRating }) => {
                             required />
                     </FormGroup>
                 </Form>
+            </div>
+
+            <div className="booking__bottom">
+                <ListGroup>
+                    <ListGroupItem className='border-0 px-0'>
+                        <h5 className='d-flex align-items-center gap-1'>
+                            ${price} <i className='ri-close-line'></i>
+                            1 person
+                        </h5>
+                        <span> ${price}</span>
+                    </ListGroupItem>
+
+                    <ListGroupItem className='border-0 px-0'>
+                        <h5>
+                            Service charge
+                        </h5>
+                        <span> ${serviceFee}</span>
+                    </ListGroupItem>
+
+                    <ListGroupItem className='border-0 px-0 total'>
+                        <h5>
+                            Total
+                        </h5>
+                        <span> ${totalAmount}</span>
+                    </ListGroupItem>
+                </ListGroup>
+
+                <Button className='btn primary__btn w-100 mt-4' onClick={handleSubmit}>
+                    Book Now
+                </Button>
             </div>
         </div>
     );
--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,9 +1,88 @@
 import React from 'react';
+import { Container, Col, Row, Button, Form, FormGroup } from 'reactstrap';
+import { Link } from 'react-router-dom';
+import registerImg from '../assets/images/register.png';
 
 const Register = () => {
+
+    const [credentials, setCredentials] = useState({
+        username: undefined,
+        email: undefined,
+        password: undefined,
+    });
+
+    const handleChange = (e) => {
+        setCredentials((prev) => ({
+            ...prev,
+            [e.target.id]: e.target.value
+        }));
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+    };
+
     return (
-        <div>Register</div>
-    )
-}
+        <section>
+            <Container>
+                <Row>
+                    <Col lg='8' className='m-auto'>
+                        <div className="register__container d-flex justify-content-between">
+                            <div className="register__img">
+                                <img src={registerImg} alt="" />
+                            </div>
+
+                            <div className="register__form">
+                                <div className="user">
+                                    <img src={userIcon} alt="" />
+                                </div>
+                                <h2>Register</h2>
+
+                                <Form onSubmit={handleSubmit}>
+                                    <FormGroup>
+                                        <input
+                                            type="text"
+                                            placeholder='Username'
+                                            id='username'
+                                            onChange={handleChange}
+                                            required />
+                                    </FormGroup>
+
+                                    <FormGroup>
+                                        <input
+                                            type="email"
+                                            placeholder='Email'
+                                            id='email'
+                                            onChange={handleChange}
+                                            required />
+                                    </FormGroup>
+
+                                    <FormGroup>
+                                        <input
+                                            type="password"
+                                            placeholder='Password'
+                                            id='password'
+                                            onChange={handleChange}
+                                            required />
+                                    </FormGroup>
+
+                                    <Button
+                                        className='btn secondary__btn auth__btn'
+                                        type='submit'>
+                                        Create Account
+                                    </Button>
+                                </Form>
+
+                                <p>Already have an account?
+                                    <Link to={'/login'}>Login</Link>
+                                </p>
+                            </div>
+                        </div>
+                    </Col>
+                </Row>
+            </Container>
+        </section>
+    );
+};
 
 export default Register;
